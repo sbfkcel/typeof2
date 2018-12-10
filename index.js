523066ecd2e46e60404ecffed7e30b4ad9b10289
单炒饭 <sbfkcel@163.com>
2018-12-10 15:17:34 +0800

    修复变量名错误

--- index.js
+++ index.js
@@ -6,5 +6,5 @@
  */
 let t,
     type = arg => (t = typeof arg) === "object" ? {}.toString.call(arg).slice(8,-1).toLowerCase() : t,
-    name = arg => (t = type(arg)) === "function" ? arg.name : temp === "undefined" ? undefined : arg.constructor.name;
+    name = arg => (t = type(arg)) === "function" ? arg.name : t === "undefined" ? undefined : arg.constructor.name;
 module.exports = arg => {return {type:type(arg),name:name(arg)}};
