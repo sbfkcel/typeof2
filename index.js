949c8d74ab012ab9d0194aa1afaf88f1404edd01
Fried rice <sbfkcel@163.com>
2020-10-21 11:34:09 +0800

    Update index.js
    精简优化代码

--- index.js
+++ index.js
@@ -7,4 +7,4 @@
 let t,
     type = arg => (t = typeof arg) === "object" ? {}.toString.call(arg).slice(8,-1).toLowerCase() : t,
     name = arg => (t = type(arg)) === "function" ? arg.name : t === "undefined" ? undefined : arg.constructor.name;
-module.exports = arg => {return {type:type(arg),name:name(arg)}};
+module.exports = arg => ({type:type(arg),name:name(arg)});
